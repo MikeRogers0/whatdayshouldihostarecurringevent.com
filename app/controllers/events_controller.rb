038d5cb7c2d76f4a6c60dbca6312136ae0967f84
Mike Rogers <me@mikerogers.io>
2015-09-30 19:01:18 +0100

    Making the homepage suggest a random reccurance

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
   def show
+    @ordinal = params[:ordinal]
+    @day = params[:day]
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,27 @@
 class HomeController < ApplicationController
   def index
-    redirect_to event_interval_path ordinal: "1st", day: "monday"
+    redirect_to event_interval_path ordinal: random_ordinal, day: random_day
+  end
+
+  private
+  def random_ordinal
+    [
+      "1st",
+      "2nd", 
+      "3rd", 
+      "4th"
+    ].sample
+  end
+
+  def random_day
+    [
+      "monday",
+      "tuesday",
+      "wednesday",
+      "thursday",
+      "friday",
+      "saturday",
+      "sunday"
+    ].sample
   end
 end
