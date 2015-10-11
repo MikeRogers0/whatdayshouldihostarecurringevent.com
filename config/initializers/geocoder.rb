f12ead53185be22f3eca2bcf5bec1b9c8eefa0ee
Mike Rogers <me@mikerogers.io>
2015-10-11 14:36:06 +0200

    giving maxmind a try - their db was pretty accurate

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,10 @@
 Geocoder.configure(
   :timeout      => 15,
-  :ip_lookup    => :telize,
+  :ip_lookup    => :maxmind,
   :cache        => Rails.cache,
   :cache_prefix => "geocoder:",
+  :maxmind => {
+    :api_key => ENV["MAXMIND_API_KEY"],
+    :service => :omni
+  },
 )
