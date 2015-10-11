9f508ea03ad67f850bfbb2e040fa2dde3a0bb98c
Mike Rogers <me@mikerogers.io>
2015-10-11 14:54:35 +0200

    Cant figure out why geocoder isnt returning anything

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,7 @@ class EventsController < ApplicationController
     @ordinal = params[:ordinal]
     @day = params[:day]
     @next_event_date = RecurringEvent.new(ordinal: @ordinal, day: @day).next
+    raise request.location.inspect
     if request.location.present?
       @conflicting_events = ConflictingEvents.new(city: request.location.city, country_code: request.location.country_code, event_date: @next_event_date)
     else
