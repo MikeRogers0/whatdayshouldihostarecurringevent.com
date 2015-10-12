c16b9fd957b959a3099771d8449ab665863dee59
Mike Rogers <me@mikerogers.io>
2015-10-12 10:58:12 +0200

    fallback for when no city is found

--- lib/ip_to_location.rb
+++ lib/ip_to_location.rb
@@ -2,7 +2,10 @@ class IpToLocation
   def initialize(remote_ip)
     request = Geoip2.city(remote_ip)
 
-    @city = request.city.names.en
+    @city = "London"
+    if request.city.present?
+      @city = request.city.names.en
+    end
     @country_code = request.country.iso_code
   end
 
